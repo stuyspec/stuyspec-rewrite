dc4e650844096c540c60ed58f532e5f74f221e44
leomet07 <leomet07@gmail.com>
2021-12-16 19:08:31 -0500

    Relocate article route for better dir structure

--- pages/api/articles/index.ts
+++ pages/api/articles/index.ts
@@ -1,12 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { RecievedArticle } from "../../ts_types/db";
+import { RecievedArticle } from "../../../ts_types/db";
 
 type Data = {
 	articles: RecievedArticle;
 };
 
-import { connectToDatabase } from "../../db_conn";
+import { connectToDatabase } from "../../../db_conn";
 
 export default async function handler(
 	req: NextApiRequest,
