11d418646332b0dc6af66e619b195dc24abf41ee
leomet07 <leomet07@gmail.com>
2021-12-16 19:42:22 -0500

    Get article by id in api

---
+++ pages/api/article/[article_id].ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { RecievedArticle } from "../../../ts_types/db";
+import { get_article_by_id } from "../../../db";
+
+type ResponseStructure = {
+	article: RecievedArticle;
+};
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<ResponseStructure>
+) {
+	const { method, query } = req;
+
+	if (method == "GET") {
+		let article_id: string = String(query.article_id);
+		let article = await get_article_by_id(article_id);
+		res.json({ article: JSON.parse(JSON.stringify(article)) });
+	}
+}
