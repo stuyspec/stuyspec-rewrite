e0b734c75833c402d7bc2cf68eb0dfea9a3855e6
anivanchen <IvanChen07@gmail.com>
2021-12-24 20:20:59 -0500

    Update Model

--- ts_types/db_types.ts
+++ ts_types/db_types.ts
@@ -2,11 +2,14 @@ import { ObjectId } from "mongodb";
 export interface RecievedArticle {
 	_id: ObjectId | string;
 	text: string;
-	title: string;
+  title: string;
+  contributors: Array<string>;
 	slug: string;
 	volume: Number;
 	issue: Number;
 	section: string;
 	summary: string;
-	cover_image : string;
+  cover_image: string;
+  cover_image_contributor: string;
+  is_published: boolean;
 }
