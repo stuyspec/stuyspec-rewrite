7ee44e5c96dddf0698c98feb079921e894a63109
dchen278 <dchen30@stuy.edu>
2021-12-27 20:42:10 -0500

    started staff page

---
+++ pages/about/staff.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const StaffPage = () => {
+  return (
+    <>
+      <Head>
+        <title>The Spectator | Staff</title>
+      </Head>
+        <div className="staff-page">
+            <div className="staff-page-header">
+                <h1>Staff</h1>
+            </div>
+        </div>
+    </>
+  );
+};
+
+export default StaffPage;
