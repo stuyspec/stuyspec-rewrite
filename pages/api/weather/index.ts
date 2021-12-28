7e8f3d8fc94d41dde125532f15d940271613fe10
dchen278 <dchen30@stuy.edu>
2021-12-28 12:57:22 -0500

    prelim weather api

---
+++ pages/api/weather/index.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<any>
+) {
+	const { method, body } = req;
+    if (method == "GET") {
+		// fetch ip address
+		const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+		console.log(ip);
+		// const weather = await fetch(
+		// 	"https://api.openweathermap.org/data/2.5/weather?q="
+		// );
+		// const weatherData = await weather.json();
+		res.status(200).json({});
+	}
+ }
