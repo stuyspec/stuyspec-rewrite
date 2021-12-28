532e7addaebf82a35215fba3c2562694f231026f
dchen278 <dchen30@stuy.edu>
2021-12-28 16:09:22 -0500

    add gh links to dev page

--- pages/about/developers.tsx
+++ pages/about/developers.tsx
@@ -8,11 +8,13 @@ const DevelopersPage = () => {
     role: string;
     year: string;
     image: string;
+    github: string;
   }> = [
     {
       name: "David Chen",
       role: "Editor",
       year: "2023",
+      github: "dchen278",
       image:
         "https://cdn.discordapp.com/attachments/854398680835948544/925432531959054346/unknown.png",
     },
@@ -20,6 +22,7 @@ const DevelopersPage = () => {
       name: "Leonid Metlitsky",
       role: "Developer",
       year: "2025",
+      github: "leomet07",
       image:
         "https://cdn.discordapp.com/avatars/426703074157920266/b48738825b6be755d98be7b97af3d449.png?size=256",
     },
@@ -27,6 +30,7 @@ const DevelopersPage = () => {
       name: "Ivan Chen",
       role: "Developer",
       year: "2024",
+      github: "anivanchen",
       image:
         "https://cdn.discordapp.com/avatars/695729168343629844/fb8e8f749485685fa9f5c62f56f330f2.png?size=256",
     },
@@ -51,9 +55,8 @@ const DevelopersPage = () => {
         <title>Developers | The Spectator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div style={{ textAlign: "center", marginTop: "1rem" }}>
+      <div id={styles.pageBody}>
         <h1>Major Contributors of The Spectator&apos;s Website</h1>
-        {/* image and description are top down */}
         <div
           style={{
             display: "flex",
@@ -80,7 +83,7 @@ const DevelopersPage = () => {
                 />
               </div>
               <h3>
-                {developer.name}
+                <a href={`https://github.com/${developer.github}`}>{developer.name}</a>
                 {truncateYear(developer.year)}
               </h3>
               <h4>{developer.role}</h4>
@@ -109,7 +112,9 @@ const DevelopersPage = () => {
                 key={key}
               >
                 <h3>
-                  {maintainer.name}
+                  <a href={`https://github.com/${maintainer.github}`}>
+                    {maintainer.name}
+                  </a>
                   {truncateYear(maintainer.year)}
                 </h3>
                 <h4>{maintainer.role}</h4>
