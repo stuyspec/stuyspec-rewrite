55e157da2eac5486ec0ca78f159bccceb73e2f12
dchen278 <dchen30@stuy.edu>
2021-12-28 22:17:02 -0500

    add icons to article page

--- pages/article/[article_slug].tsx
+++ pages/article/[article_slug].tsx
@@ -6,67 +6,81 @@ import { get_article_by_slug } from "../../db";
 import { NextPageContext } from "next";
 import styles from "../../styles/[article_slug].module.css";
 import { all_sections } from "../../globals/globals";
+import ShareButton from "../../components/ShareButton";
+
 interface Props {
-	article: ReceivedArticle;
+  article: ReceivedArticle;
 }
 
 const Article = (props: Props) => {
-	const {
-		text,
-		title,
-		cover_image,
-		section_id,
-		contributors,
-		cover_image_contributor,
-	} = props.article;
-	const section = all_sections[section_id];
-	return (
-		<div>
-			<Head>
-				<title>{title}</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+  const {
+    text,
+    title,
+    cover_image,
+    section_id,
+    contributors,
+    cover_image_contributor,
+  } = props.article;
+  const section = all_sections[section_id];
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-			<main id={styles.main}>
-				<h1>{title}</h1>
-				<h3 className={styles.authors}>
-					{section} | By {contributors.join(", ")}
-				</h3>
-				<div id={styles.cover_image_div}>
-					<img
-						width={"100%"}
-						id={styles.cover_image}
-						src={cover_image}
-						alt="Cover Image"
-					/>
-				</div>
-				<p className={styles.cover_image_contributor}>
-					By {cover_image_contributor}
-				</p>
+      <main id={styles.main}>
+        <h1>{title}</h1>
+        <h3 className={styles.authors}>
+          {section} | By {contributors.join(", ")}
+        </h3>
+        <ShareButton
+          url={`https://stuyspec.com/article/${props.article.slug}`}
+          provider="facebook"
+        />
+        <ShareButton
+          url={`https://stuyspec.com/article/${props.article.slug}`}
+          provider="twitter"
+        />
+        <ShareButton
+          url={`https://stuyspec.com/article/${props.article.slug}`}
+          provider="linkedin"
+        />
+        <div id={styles.cover_image_div}>
+          <img
+            width={"100%"}
+            id={styles.cover_image}
+            src={cover_image}
+            alt="Cover Image"
+          />
+        </div>
+        <p className={styles.cover_image_contributor}>
+          By {cover_image_contributor}
+        </p>
 
-				<div
-					id={styles.content}
-					dangerouslySetInnerHTML={{ __html: text }}
-				></div>
-			</main>
-		</div>
-	);
+        <div
+          id={styles.content}
+          dangerouslySetInnerHTML={{ __html: text }}
+        ></div>
+      </main>
+    </div>
+  );
 };
 
 export default Article;
 
 export async function getServerSideProps(context: NextPageContext) {
-	const article_slug: string = String(context.query.article_slug);
+  const article_slug: string = String(context.query.article_slug);
 
-	let article = await get_article_by_slug(article_slug);
-	if (article) {
-		return {
-			props: { article: JSON.parse(JSON.stringify(article)) },
-		};
-	} else {
-		return {
-			notFound: true,
-			props: { attempted_identifier: article_slug },
-		};
-	}
+  let article = await get_article_by_slug(article_slug);
+  if (article) {
+    return {
+      props: { article: JSON.parse(JSON.stringify(article)) },
+    };
+  } else {
+    return {
+      notFound: true,
+      props: { attempted_identifier: article_slug },
+    };
+  }
 }
