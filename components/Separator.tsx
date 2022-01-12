d15724c61e4eed4b0eba9ad736e7301e3003dcbc
anivanchen <IvanChen07@gmail.com>
2022-01-12 09:41:51 -0500

    Started styling home page. Updated footer.

---
+++ components/Separator.tsx
@@ -0,0 +1,14 @@
+const Separator = () => {
+	return (
+    <>
+      <div style={{
+        width: "100%",
+        height: "0.125rem",
+        marginTop: "1rem",
+        backgroundColor: "var(--medium-grey)"
+      }} />
+		</>
+	);
+};
+
+export default Separator;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { get_articles } from "../db";
 import { ReceivedArticle } from "../ts_types/db_types";
+import { all_sections } from "../globals/globals";
+import Separator from "../components/Separator";
 interface Props {
   articles: [ReceivedArticle];
 }
@@ -13,16 +16,21 @@ const Home = (props: Props) => {
   const articles = props.articles;
   articles.forEach((i) => {
     displayArticles.push(
-      <div className={styles.displayed_article} key={String(i._id)}>
-        <Link href={"/article/" + i.slug} passHref>
+      <div id={styles.article} key={String(i._id)}>
+        <Link href={ "/article/" + i.slug } passHref>
           <div>
-            <h2 className={styles.mini_article_title}>{i.title}</h2>
-            <p>{i.summary}</p>
+            <h2 id={styles.title}>{ i.title }</h2>
+            <p id={styles.articleInfo}>{ i.contributors.join(", ") }</p>
+            <p id={styles.summary}>{ i.summary }</p>
+            <p id={styles.articleInfo}>{ all_sections[i.section_id] }</p>
           </div>
         </Link>
       </div>
     );
   });
+
+  const heroArticle = articles[0];
+
   return (
     <div>
       <Head>
@@ -32,10 +40,48 @@ const Home = (props: Props) => {
       </Head>
 
       <main id={styles.main}>
-        {/* <h1 id={styles.title}>
-					Welcome to the stuy spec rewrite in Typescript
-				</h1> */}
-        <div>{displayArticles}</div>
+        <div id={styles.landingScreen}>
+
+          <Link passHref href={"/article/" + heroArticle.slug}>
+            <div id={styles.heroStory}>
+              <div id={styles.heroImageContainer}>
+                <img id={styles.heroImage} src={heroArticle.cover_image} />
+                <div id={styles.departmentBar}>{ all_sections[heroArticle.section_id] }</div>
+              </div>
+                
+              <h1>{heroArticle.title}</h1>
+              <p id={styles.writers}>{heroArticle.contributors.join(", ")}</p>
+              <p id={styles.summary}>{heroArticle.summary}</p>
+              <Image src="/images/down-arrow.svg" width="24px" height="24px" />
+            </div>
+          </Link>
+
+          <div id={styles.infoBar}>
+            <Separator />
+            <div id={styles.announcements}>
+              <div id={styles.countdown}>
+                <p><h1>h</h1>days until {"christmas break"}</p>
+              </div>
+            </div>
+            <div style={{transform: "rotate(90deg)"}}><Separator /></div>
+            <div id={styles.weather}>
+              snow yay
+            </div>
+            <Separator />
+          </div>
+
+          <div id={styles.latestArticles}>
+            <h1 id={styles.heading}>Latest</h1>
+            <Separator />
+            <div>{displayArticles.slice(1)}</div>
+          </div>
+        </div>
+        <div id={styles.articles}>
+
+        </div>
+        <div id={styles.articles}>
+
+        </div>
       </main>
     </div>
   );
