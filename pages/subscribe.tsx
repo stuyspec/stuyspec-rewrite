de654b2783773c27a6108f5b8672bd6889944222
anivanchen <IvanChen07@gmail.com>
2022-01-14 22:43:05 -0500

    Separated Subscribe form into component

--- pages/subscribe.tsx
+++ pages/subscribe.tsx
@@ -1,36 +1,8 @@
-import { useState } from "react";
 import Head from "next/head";
 import styles from "../styles/Subscribe.module.css";
+import SubscribeForm from "../components/SubscribeForm";
 
 const Subscribe = () => {
-	const [email, setEmail] = useState("");
-	const [subscribeState, setSubscribeState] = useState("idle");
-	const [errorMsg, setErrorMsg] = useState(null);
-
-	const subscribe = async (e: any) => {
-		e.preventDefault();
-		setSubscribeState("Loading");
-
-		try {
-			const r = await fetch("/api/subscribe", {
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify({ email }),
-			});
-			const rjson = await r.json();
-			console.log(rjson);
-			if (rjson.message == "success") {
-				setSubscribeState("Success");
-				setEmail("");
-			} else {
-				throw new Error(rjson.error.title);
-			}
-		} catch (e: any) {
-			console.error(e);
-			setErrorMsg(e.message);
-			setSubscribeState("Error");
-		}
-	};
 
 	return (
 		<>
@@ -45,32 +17,7 @@ const Subscribe = () => {
 				<p id={styles.text}>
 					Subscribe to The Spectator&apos;s biweekly newsletter!
 				</p>
-				<input
-					required
-					name="email"
-					type="email"
-					id={styles.input}
-					placeholder="Email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-				<br />
-				<button
-					disabled={subscribeState === "Loading"}
-					type="submit"
-					id={styles.button}
-					onClick={subscribe}
-				>
-					Subscribe
-				</button>
-				{subscribeState === "Error" && (
-					<p id={styles.text}>Error: {errorMsg}</p>
-				)}
-				{subscribeState === "Success" && (
-					<p id={styles.text}>
-						Awesome sauce!!! You&apos;ve been subscribed!
-					</p>
-				)}
+        <SubscribeForm />
 			</main>
 		</>
 	);
