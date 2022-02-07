eb2888691e6b25048ad2295074202fcd48d41906
anivanchen <IvanChen07@gmail.com>
2022-02-06 22:08:58 -0500

    Update department component dependancy styling

--- pages/department/[department].tsx
+++ pages/department/[department].tsx
@@ -1,14 +1,25 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Department, ReceivedArticle } from "../../ts_types/db_types";
+import { Department, ReceivedArticle, mongoObjectId } from "../../ts_types/db_types";
 import { get_articles_by_department } from "../../db";
 import { NextPageContext } from "next";
 import styles from "../../styles/[department].module.css";
+import gridStyles from "../../styles/ItemGrid.module.css";
 import Link from "next/link";
+import Sidebar from "../../components/Sidebar";
 
 interface Props {
 	articles: ReceivedArticle[];
 	department: string;
+}
+
+function dateFromID(objectId: mongoObjectId) {
+	objectId = String(objectId);
+	return new Date(parseInt(objectId.substring(0, 8), 16) * 1000)
+		.toString()
+		.split(" ")
+		.slice(0, 4)
+		.join(" ");
 }
 
 const Article = (props: Props) => {
@@ -19,34 +30,59 @@ const Article = (props: Props) => {
 				<title>{props.department}</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-
-			<main id={styles.main}>
-				<h1 id={styles.departmentTitle}> {props.department} </h1>
-				<div id={styles.devDisplay}>
-					<h2>Development Display</h2>
-					{props.articles.length > 0 ? (
-						<div>
-							{props.articles.map((v) => (
-								<Link
-									key={v._id as any}
-									href={"/article/" + v.slug}
-									passHref
-								>
-									<div>
-										<h3>{v.title}</h3>
-										<h4>By {v.contributors.join(", ")}</h4>
-									</div>
-								</Link>
-							))}
-						</div>
-					) : (
-						<div>
-							<h2>
-								No articles were found under that department
-							</h2>
-						</div>
-					)}
-				</div>
+      <main id={styles.container}>
+        <div id={styles.sidebar}>
+          <Sidebar />
+        </div>
+        <h1 id={styles.department}>{props.department.charAt(0).toUpperCase() + props.department.slice(1)}</h1>
+        <div className={gridStyles.container}>
+            <div id={gridStyles.grid}>
+              {props.articles.map((page, key) => (
+                <div key={key}>
+                  <img id={gridStyles.image} src={page.cover_image} />
+                  <a href={`/article/${page.slug}`}>
+                  <h2 id={styles.articleTitle} className="discrete-link">{page.title}</h2>
+                  </a>
+                  <div
+                    id={styles.inline}
+                    style={{ fontFamily: "var(--secondary-font)" }}
+                  >
+                    {page.contributors.map(
+                      (contributor: string, index: number) => {
+                        let separator =
+                          index ===
+                          page.contributors.length - 1
+                            ? ""
+                            : ",";
+                          
+                        return (
+                          <div key={index}>
+                            <p
+                              id={styles.articleInfoWriters}
+                              style={{
+                                color: "var(--primary)",
+                                fontFamily: "var(--secondary-font)",
+                              }}
+                              className="discrete-link"
+                              key={contributor}
+                            >
+                              {contributor}
+                              {separator}
+                              &nbsp;
+                            </p>
+                          </div>
+                        );
+                      }
+                    )}
+                  </div>
+                  <p id={styles.focusSentence}>{page.summary}</p>
+                  <p id={styles.articleInfoDate}>
+                      {dateFromID(page._id)}
+                    </p>
+              </div>
+              ))}
+            </div>
+          </div>
 			</main>
 		</div>
 	);
