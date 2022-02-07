ec1abb27733176f4b6ef6ac06c4c0891fbae5d1a
anivanchen <IvanChen07@gmail.com>
2022-02-06 22:05:53 -0500

    Update Sidebar to get correct page name

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,13 @@ import { useRouter } from "next/router";
 import styles from "../styles/Sidebar.module.css";
 
 const Sidebar = () => {
-  let pageName = useRouter().pathname.split("/").pop();
+
+  let pageName: string | undefined;
+
+  if (typeof window !== "undefined") { 
+    pageName = window.location.href.split("/").pop();
+  }
+
   if (pageName != undefined) {
     pageName = pageName.charAt(0).toUpperCase() + pageName.slice(1);
   }
