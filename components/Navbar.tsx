ae54d82b291e38a7858eda0782052d4e7a317306
dchen278 <dchen30@stuy.edu>
2022-03-20 11:45:52 -0400

    add basic modal

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,31 +2,31 @@ import styles from "../styles/Navbar.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import ThemeToggle from "./ThemeToggle";
-import SubscribeButton from "./SubscribeButton";
 import CollapsibleSearch from "./CollapsibleSearch";
+import SubscribeModal from "./SubscribeModal";
+import { useState } from "react";
+import SubscribeButton from "../styles/SubscribeButton.module.css";
+
 const Navbar = () => {
-	function toggleMenu() {}
+  const [isOpen, setIsOpen] = useState(false);
+  function toggleMenu() {}
 
-	function enableSearch() {
-		var buttons = Array.from(
-			document.getElementsByClassName(
-				"button"
-			) as HTMLCollectionOf<HTMLElement>
-		);
-		for (let ele of buttons) {
-			ele.style.display = "none";
-		}
+  function enableSearch() {
+    var buttons = Array.from(
+      document.getElementsByClassName("button") as HTMLCollectionOf<HTMLElement>
+    );
+    for (let ele of buttons) {
+      ele.style.display = "none";
+    }
 
-		Array.from(
-			document.getElementsByClassName(
-				"search"
-			) as HTMLCollectionOf<HTMLElement>
-		)[0].style.display = "inherit";
-	}
+    Array.from(
+      document.getElementsByClassName("search") as HTMLCollectionOf<HTMLElement>
+    )[0].style.display = "inherit";
+  }
 
-	return (
-		<nav id={styles.nav}>
-			{/* <div id={styles.hamburgerMenu} className="button">
+  return (
+    <nav id={styles.nav}>
+      {/* <div id={styles.hamburgerMenu} className="button">
 				<Image
 					src="/images/hamburger-menu.svg"
 					width="24px"
@@ -38,21 +38,24 @@ const Navbar = () => {
 				/>
 			</div> */}
 
-			<span id={styles.logo_container}>
-				<Link passHref href="/">
-					<div>
-						<span id={styles.logo_the}>The </span>Spectator
-					</div>
-				</Link>
-			</span>
-			<div id={styles.subscribe_parent}>
-				<SubscribeButton />
-			</div>
+      <span id={styles.logo_container}>
+        <Link passHref href="/">
+          <div>
+            <span id={styles.logo_the}>The </span>Spectator
+          </div>
+        </Link>
+      </span>
+      <div id={styles.subscribe_parent}>
+        <button className={styles.primaryBtn} onClick={() => setIsOpen(true)}>
+          Open Modal
+        </button>
+        {isOpen && <SubscribeModal setIsOpen={setIsOpen} />}
+      </div>
 
-			<ThemeToggle />
-			<CollapsibleSearch />
-		</nav>
-	);
+      <ThemeToggle />
+      <CollapsibleSearch />
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ components/SubscribeModal.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "../styles/Modal.module.css";
+import { RiCloseLine } from "react-icons/ri";
+
+const Modal = ({ setIsOpen }) => {
+  return (
+    <>
+      <div className={styles.darkBG} onClick={() => setIsOpen(false)} />
+      <div className={styles.centered}>
+        <div className={styles.modal}>
+          <div className={styles.modalHeader}>
+            <h5 className={styles.heading}>Dialog</h5>
+          </div>
+          <button className={styles.closeBtn} onClick={() => setIsOpen(false)}>
+            <RiCloseLine style={{ marginBottom: "-3px" }} />
+          </button>
+          <div className={styles.modalContent}>
+            Are you sure you want to delete the item?
+          </div>
+          <div className={styles.modalActions}>
+            <div className={styles.actionsContainer}>
+              <button className={styles.deleteBtn} onClick={() => setIsOpen(false)}>
+                Delete
+              </button>
+              <button
+                className={styles.cancelBtn}
+                onClick={() => setIsOpen(false)}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
