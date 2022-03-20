f10707af110a32dcb3d8e6da5eeca9d4d7269805
dchen278 <dchen30@stuy.edu>
2022-03-20 11:55:01 -0400

    ;(

--- components/SubscribeModal.tsx
+++ components/SubscribeModal.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../styles/Modal.module.css";
 import { RiCloseLine } from "react-icons/ri";
+import SubscribeForm from "./SubscribeForm";
 
 const Modal = ({ setIsOpen }) => {
   return (
@@ -9,26 +10,13 @@ const Modal = ({ setIsOpen }) => {
       <div className={styles.centered}>
         <div className={styles.modal}>
           <div className={styles.modalHeader}>
-            <h5 className={styles.heading}>Dialog</h5>
+            <h5 className={styles.heading}>Newsletter Subscription</h5>
           </div>
           <button className={styles.closeBtn} onClick={() => setIsOpen(false)}>
             <RiCloseLine style={{ marginBottom: "-3px" }} />
           </button>
-          <div className={styles.modalContent}>
-            Are you sure you want to delete the item?
-          </div>
-          <div className={styles.modalActions}>
-            <div className={styles.actionsContainer}>
-              <button className={styles.deleteBtn} onClick={() => setIsOpen(false)}>
-                Delete
-              </button>
-              <button
-                className={styles.cancelBtn}
-                onClick={() => setIsOpen(false)}
-              >
-                Cancel
-              </button>
-            </div>
+          <div className={styles.centered}>
+            <SubscribeForm />
           </div>
         </div>
       </div>
