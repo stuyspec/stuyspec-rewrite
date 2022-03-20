cdc97d0bc61b1c779ad9848ad863e924f38313ec
dchen278 <dchen30@stuy.edu>
2022-03-20 11:59:20 -0400

    fix modal

--- components/SubscribeForm.tsx
+++ components/SubscribeForm.tsx
@@ -2,65 +2,62 @@ import styles from "../styles/Subscribe.module.css";
 import { useState } from "react";
 import subscribe_button_styles from "../styles/SubscribeButton.module.css";
 const SubscribeForm = () => {
-	const [email, setEmail] = useState("");
-	const [subscribeState, setSubscribeState] = useState("idle");
-	const [errorMsg, setErrorMsg] = useState(null);
+  const [email, setEmail] = useState("");
+  const [subscribeState, setSubscribeState] = useState("idle");
+  const [errorMsg, setErrorMsg] = useState(null);
 
-	const subscribe = async (e: any) => {
-		e.preventDefault();
-		setSubscribeState("Loading");
+  const subscribe = async (e: any) => {
+    e.preventDefault();
+    setSubscribeState("Loading");
 
-		try {
-			const r = await fetch("/api/subscribe", {
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify({ email }),
-			});
-			const rjson = await r.json();
-			console.log(rjson);
-			if (rjson.message == "success") {
-				setSubscribeState("Success");
-				setEmail("");
-			} else {
-				throw new Error(rjson.error.title);
-			}
-		} catch (e: any) {
-			console.error(e);
-			setErrorMsg(e.message);
-			setSubscribeState("Error");
-		}
-	};
+    try {
+      const r = await fetch("/api/subscribe", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email }),
+      });
+      const rjson = await r.json();
+      console.log(rjson);
+      if (rjson.message == "success") {
+        setSubscribeState("Success");
+        setEmail("");
+      } else {
+        throw new Error(rjson.error.title);
+      }
+    } catch (e: any) {
+      console.error(e);
+      setErrorMsg(e.message);
+      setSubscribeState("Error");
+    }
+  };
 
-	return (
-		<>
-			<input
-				required
-				name="email"
-				type="email"
-				id={styles.input}
-				placeholder="Email"
-				value={email}
-				onChange={(e) => setEmail(e.target.value)}
-			/>
-			<br />
-			<button
-				disabled={subscribeState === "Loading"}
-				type="submit"
-				id={subscribe_button_styles.subscribe}
-				onClick={subscribe}
-			>
-				Subscribe
-			</button>
-			{subscribeState === "Error" && (
-				<p id={styles.text}>Error: {errorMsg}</p>
-			)}
-			{subscribeState === "Success" && (
-				<p id={styles.text}>
-					Awesome sauce!!! You&apos;ve been subscribed!
-				</p>
-			)}
-		</>
-	);
+  return (
+    <>
+      <br />
+      <input
+        required
+        name="email"
+        type="email"
+        id={styles.input}
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+      <button
+        disabled={subscribeState === "Loading"}
+        type="submit"
+        id={subscribe_button_styles.subscribe}
+        onClick={subscribe}
+      >
+        Subscribe
+      </button>
+      {subscribeState === "Error" && <p id={styles.text}>Error: {errorMsg}</p>}
+      {subscribeState === "Success" && (
+        <p id={styles.text}>Awesome sauce!!! You&apos;ve been subscribed!</p>
+      )}
+    </>
+  );
 };
 
 export default SubscribeForm;
