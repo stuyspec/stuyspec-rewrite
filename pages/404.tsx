fa044d3bc3aeabba313c12d3059ffedd48c79491
anivanchen <ivanchen07@gmail.com>
2022-06-23 13:05:46 -0400

    Update 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/404.module.css";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 const Fourzerofour = (props: any) => {
 	const [location, setLocation] = useState("");
@@ -16,10 +17,13 @@ const Fourzerofour = (props: any) => {
 
 			<main id={styles.main}>
 				<h1>Error!</h1>
-				{location ? (
-					<h2>{location} doesn&apos;t exist!</h2>
+                {location ? (
+                    <>
+                        <h2>The page <code>{location}</code> cannot be found.</h2>
+                        <Link passHref href="/"><div id={styles.button}>Back to Home</div></Link>
+                    </>
 				) : (
-					<h2>This page doesn&apos;t exist! </h2>
+					<h2>This page doesn&apos;t exist!</h2>
 				)}
 			</main>
 		</div>
