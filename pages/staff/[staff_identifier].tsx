9928f9a207c8c501060391f8e9cdef23802dd6ba
leomet07 <leomet07@gmail.com>
2022-07-13 11:49:39 -0400

    Staff page uses slug by default; ID identification still possible

--- pages/staff/[staff_identifier].tsx
+++ pages/staff/[staff_identifier].tsx
@@ -3,9 +3,10 @@ import { mongoObjectId, ReceivedStaff } from "../../ts_types/db_types";
 
 import { NextPageContext } from "next";
 import styles from "../../styles/[staff_id].module.css";
-import { get_staff_by_id } from "../../db";
+import { get_staff_by_id, get_staff_by_slug } from "../../db";
+
 interface Props {
-	staff_id: mongoObjectId;
+	staff_identifier: mongoObjectId;
 	staff: ReceivedStaff;
 }
 
@@ -20,10 +21,10 @@ const StaffMember = (props: Props) => {
 
 			<main id={styles.main}>
 				<h1>Staff ID: {staff_member._id}</h1>
+				<h1>Slug (Legacy): {staff_member.slug}</h1>
 				<h1>Name: {staff_member.name}</h1>
 				<h1>Email: {staff_member.email}</h1>
 				<h2>Description: {staff_member.description}</h2>
-				<h3>Slug (Legacy): {staff_member.slug}</h3>
 			</main>
 		</div>
 	);
@@ -32,20 +33,25 @@ const StaffMember = (props: Props) => {
 export default StaffMember;
 
 export async function getServerSideProps(context: NextPageContext) {
-	let staff_id = String(context.query.staff_id);
+	let staff_identifier = String(context.query.staff_identifier);
 
-	let staff = await get_staff_by_id(staff_id);
+	let staff: ReceivedStaff;
+	if (context.query.identifier_type == "id") {
+		staff = await get_staff_by_id(staff_identifier);
+	} else {
+		staff = await get_staff_by_slug(staff_identifier); // Getting staff by slug is default, for legacy support
+	}
 	if (staff) {
 		return {
 			props: {
 				staff: JSON.parse(JSON.stringify(staff)),
-				staff_id: staff_id,
+				staff_identifier: staff_identifier,
 			},
 		};
 	} else {
 		return {
 			notFound: true,
-			props: { attempted_identifier: staff_id },
+			props: { attempted_identifier: staff_identifier },
 		};
 	}
 }
