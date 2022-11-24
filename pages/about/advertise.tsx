3c646810e89a3f18ccf0dc731e60a5fb280df65f
leomet07 <leomet07@gmail.com>
2022-11-23 21:25:57 -0500

    Lint

--- pages/about/advertise.tsx
+++ pages/about/advertise.tsx
@@ -1,103 +1,210 @@
-
 import Head from "next/head";
-import styles from "../../styles/About.module.css"
+import styles from "../../styles/About.module.css";
 import { ReceivedStaff } from "../../ts_types/db_types";
 import { get_staff_by_position } from "../../db";
 
 interface Props {
-  manager: ReceivedStaff;
+	manager: ReceivedStaff;
 }
 
 const Advertise = (props: Props) => {
+	const { name, email } = props.manager;
+	const mailto = "mailto:" + email;
 
-  const { name, email } = props.manager;
-  const mailto = "mailto:" + email;
-  
-  return (
-    <>
-      <Head>
-        <title>Advertise | The Spectator</title>
-      </Head>
-      <div
-        id={styles.container}
-      >
-        <div style={{ margin: "auto" }}>
-          <h1 style={{ fontSize: "var(--large-text)" }}>Advertise</h1>
-          
-          <h2 style={{ fontSize: "var(--medium-text)" }}>Local Advertising</h2>
-          <p style={{ marginTop: "1rem", marginLeft: "2rem", fontFamily: "Georgia" }}>
-            Local advertisers are classified as all those companies
-            either headquartered within New York City or whose New
-            York City local branch is placing an advertisement. The
-            best way to contact an advertising representative is to {" "}
-            <a href="mainto:specbusiness@gmail.com" className="link">e-mail the business manager</a>.
-            For information on rates and our publication schedule,
-            please see the advertising forms below.
-          </p>
+	return (
+		<>
+			<Head>
+				<title>Advertise | The Spectator</title>
+			</Head>
+			<div id={styles.container}>
+				<div style={{ margin: "auto" }}>
+					<h1 style={{ fontSize: "var(--large-text)" }}>Advertise</h1>
 
-          <h2 style={{ fontSize: "var(--medium-text)" }}>National Advertising</h2>
-          <p style={{ marginTop: "1rem", marginLeft: "2rem", fontFamily: "Georgia" }}>
-            Please direct all national advertising orders and
-            questions to our business desk:
-            <p style={{ marginTop: "1rem", marginLeft: "2rem", fontFamily: "Georgia" }}>
-              Email: <a href="mainto:specbusiness@gmail.com" className="link">business@stuyspec.com</a>
-            </p>
-            <p style={{ marginTop: "1rem", marginLeft: "2rem", fontFamily: "Georgia" }}>
-              Fax: 212-587-3874, attn. The Spectator
-            </p>
-            <p style={{ marginTop: "1rem", marginLeft: "2rem", fontFamily: "Georgia" }}>
-              Phone: 212-312-4800 ext. 2601
-            </p>
-          </p>
+					<h2 style={{ fontSize: "var(--medium-text)" }}>
+						Local Advertising
+					</h2>
+					<p
+						style={{
+							marginTop: "1rem",
+							marginLeft: "2rem",
+							fontFamily: "Georgia",
+						}}
+					>
+						Local advertisers are classified as all those companies
+						either headquartered within New York City or whose New
+						York City local branch is placing an advertisement. The
+						best way to contact an advertising representative is to{" "}
+						<a
+							href="mainto:specbusiness@gmail.com"
+							className="link"
+						>
+							e-mail the business manager
+						</a>
+						. For information on rates and our publication schedule,
+						please see the advertising forms below.
+					</p>
 
-          <h2 style={{ fontSize: "var(--medium-text)" }}>Student Groups</h2>
-          <p style={{ marginTop: "1rem", marginLeft: "2rem", fontFamily: "Georgia" }}>
-            All Clubs and Pubs are offered discounted advertising rates in
-            The Spectator! In order to place an ad, a representative
-            (a president or cabinet member) must email {" "}
-            <a href="mainto:specbusiness@gmail.com" className="link">business@stuyspec.com</a>
-            {" "} with the following things:
-            <p style={{ marginTop: "1rem", marginLeft: "2rem", fontFamily: "Georgia" }}>
-              Group Name
-            </p>
-            <p style={{ marginTop: "1rem", marginLeft: "2rem", fontFamily: "Georgia" }}>
-              Size of ad requested
-            </p>
-            <p style={{ marginTop: "1rem", marginLeft: "2rem", fontFamily: "Georgia" }}>
-              The date they would like the advertisement to appear
-            </p>
-            <p style={{ marginTop: "1rem", marginLeft: "2rem", fontFamily: "Georgia" }}>
-              A digital copy of the ad in JPG or PDF format
-            </p>
-          </p>
-          <p style={{ marginTop: "1rem", marginLeft: "2rem", fontFamily: "Georgia" }}>
-            Large ad sizes are available. Please contact {" "}
-            <a href="mainto:specbusiness@gmail.com" className="link">business@stuyspec.com</a>
-            {" "} for more details.
-          </p>
+					<h2 style={{ fontSize: "var(--medium-text)" }}>
+						National Advertising
+					</h2>
+					<p
+						style={{
+							marginTop: "1rem",
+							marginLeft: "2rem",
+							fontFamily: "Georgia",
+						}}
+					>
+						Please direct all national advertising orders and
+						questions to our business desk:
+						<p
+							style={{
+								marginTop: "1rem",
+								marginLeft: "2rem",
+								fontFamily: "Georgia",
+							}}
+						>
+							Email:{" "}
+							<a
+								href="mainto:specbusiness@gmail.com"
+								className="link"
+							>
+								business@stuyspec.com
+							</a>
+						</p>
+						<p
+							style={{
+								marginTop: "1rem",
+								marginLeft: "2rem",
+								fontFamily: "Georgia",
+							}}
+						>
+							Fax: 212-587-3874, attn. The Spectator
+						</p>
+						<p
+							style={{
+								marginTop: "1rem",
+								marginLeft: "2rem",
+								fontFamily: "Georgia",
+							}}
+						>
+							Phone: 212-312-4800 ext. 2601
+						</p>
+					</p>
 
-          <h2 style={{ fontSize: "var(--medium-text)" }}>Contact Business Directly</h2>
-          <p style={{ marginTop: "1rem", marginLeft: "2rem", fontFamily: "Georgia" }}>
-            Manager: {name}
-            <div style={{margin: "4px"}} />
-            Email: <a href={mailto} className="link">{email}</a>
-          </p>
-        </div>
-      </div>
-    </>
-  );
+					<h2 style={{ fontSize: "var(--medium-text)" }}>
+						Student Groups
+					</h2>
+					<p
+						style={{
+							marginTop: "1rem",
+							marginLeft: "2rem",
+							fontFamily: "Georgia",
+						}}
+					>
+						All Clubs and Pubs are offered discounted advertising
+						rates in The Spectator! In order to place an ad, a
+						representative (a president or cabinet member) must
+						email{" "}
+						<a
+							href="mainto:specbusiness@gmail.com"
+							className="link"
+						>
+							business@stuyspec.com
+						</a>{" "}
+						with the following things:
+						<p
+							style={{
+								marginTop: "1rem",
+								marginLeft: "2rem",
+								fontFamily: "Georgia",
+							}}
+						>
+							Group Name
+						</p>
+						<p
+							style={{
+								marginTop: "1rem",
+								marginLeft: "2rem",
+								fontFamily: "Georgia",
+							}}
+						>
+							Size of ad requested
+						</p>
+						<p
+							style={{
+								marginTop: "1rem",
+								marginLeft: "2rem",
+								fontFamily: "Georgia",
+							}}
+						>
+							The date they would like the advertisement to appear
+						</p>
+						<p
+							style={{
+								marginTop: "1rem",
+								marginLeft: "2rem",
+								fontFamily: "Georgia",
+							}}
+						>
+							A digital copy of the ad in JPG or PDF format
+						</p>
+					</p>
+					<p
+						style={{
+							marginTop: "1rem",
+							marginLeft: "2rem",
+							fontFamily: "Georgia",
+						}}
+					>
+						Large ad sizes are available. Please contact{" "}
+						<a
+							href="mainto:specbusiness@gmail.com"
+							className="link"
+						>
+							business@stuyspec.com
+						</a>{" "}
+						for more details.
+					</p>
+
+					<h2 style={{ fontSize: "var(--medium-text)" }}>
+						Contact Business Directly
+					</h2>
+					<p
+						style={{
+							marginTop: "1rem",
+							marginLeft: "2rem",
+							fontFamily: "Georgia",
+						}}
+					>
+						Manager: {name}
+						<div style={{ margin: "4px" }} />
+						Email:{" "}
+						<a href={mailto} className="link">
+							{email}
+						</a>
+					</p>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export async function getServerSideProps() {
-
-  let manager = await get_staff_by_position("business manager");
-    if (manager) {
-        return {
-            props: { manager: JSON.parse(JSON.stringify(manager)) }
-        }
-    } else return {props: {manager: {name: "Error: Not found", email: "Error: Not found"}}}
-//   } else { return { notFound: true } }
-  
+	let manager = await get_staff_by_position("business manager");
+	if (manager) {
+		return {
+			props: { manager: JSON.parse(JSON.stringify(manager)) },
+		};
+	} else
+		return {
+			props: {
+				manager: {
+					name: "Error: Not found",
+					email: "Error: Not found",
+				},
+			},
+		};
+	//   } else { return { notFound: true } }
 }
 
 export default Advertise;
--- utils/groupArticles.ts
+++ utils/groupArticles.ts
@@ -1,19 +1,21 @@
 import { ReceivedArticle } from "../ts_types/db_types";
 
 function groupByImageExists(articles: ReceivedArticle[]) {
+	const articlesWithPhotos: ReceivedArticle[] = [];
+	const articlesWithoutPhotos: ReceivedArticle[] = [];
 
-    const articlesWithPhotos: ReceivedArticle[] = [];
-    const articlesWithoutPhotos: ReceivedArticle[] = [];
-    
-    articles.forEach(article => {
-        if (article.cover_image) {
-            articlesWithPhotos.push(article);
-        } else {
-            articlesWithoutPhotos.push(article);
-        }
-    })
+	articles.forEach((article) => {
+		if (article.cover_image) {
+			articlesWithPhotos.push(article);
+		} else {
+			articlesWithoutPhotos.push(article);
+		}
+	});
 
-    return {withPhotos: articlesWithPhotos, withoutPhotos: articlesWithoutPhotos}
-  }
-  
-  export default groupByImageExists;
+	return {
+		withPhotos: articlesWithPhotos,
+		withoutPhotos: articlesWithoutPhotos,
+	};
+}
+
+export default groupByImageExists;
