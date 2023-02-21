9f214c00209ad6fc47a7101897166823f09c2cd0
leomet07 <leomet07@gmail.com>
2023-02-21 14:18:40 -0500

    Create MixedArticleDisplay and make homepage display it

---
+++ components/MixedArticleDisplay.tsx
@@ -0,0 +1,134 @@
+import {
+	DepartmentsArray,
+	DepartmentsArrayDisplay,
+	ReceivedArticle,
+} from "../ts_types/db_types";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/MixedArticleDisplay.module.css";
+import groupByImageExists from "../utils/groupArticles";
+import generate_contributors_jsx from "./GenerateContributorsJSX";
+
+function CenterArticle(props: {
+	article: ReceivedArticle;
+	display_image?: boolean;
+}) {
+	const article = props.article;
+	return (
+		<div className={styles.article} key={String(article._id)}>
+			{props.display_image ? (
+				<Link passHref href={"/article/" + article.slug}>
+					<div className={styles.centerImageContainer}>
+						<Image
+							alt="big image"
+							className={styles.centerImage}
+							src={article.cover_image}
+							sizes="800px"
+							quality={90}
+							fill
+						/>
+					</div>
+				</Link>
+			) : (
+				<></>
+			)}
+			<Link
+				href={`/department/${DepartmentsArray[article.section_id]}`}
+				passHref
+			>
+				<p
+					id={styles.articleInfo}
+					className={styles.department + " discrete-link"}
+					style={{
+						fontFamily: "var(--secondary-font)",
+					}}
+				>
+					{DepartmentsArrayDisplay[article.section_id]}
+				</p>
+			</Link>
+			<Link passHref href={"/article/" + article.slug}>
+				<h2 id={styles.title} className="discrete-link">
+					{article.title}
+				</h2>
+			</Link>
+			<p
+				className={styles.authors}
+				style={{
+					fontFamily: "var(--secondary-font)",
+				}}
+			>
+				By&nbsp;
+				{generate_contributors_jsx(article.contributors)}
+			</p>
+			<Link passHref href={"/article/" + article.slug}>
+				<p id={styles.summary} className="discrete-link">
+					{article.summary}
+				</p>
+			</Link>
+
+			<p className={styles.article_volume_issue}>
+				<Link href={`/volume/${article.volume}/issue/${article.issue}`}>
+					Issue {article.issue}, Volume {article.volume}
+				</Link>
+			</p>
+		</div>
+	);
+}
+
+export default function MixedArticleDisplay(props: {
+	articles: ReceivedArticle[];
+}) {
+	const grouping = groupByImageExists(props.articles);
+	const articlesWithPhotos: ReceivedArticle[] = grouping["withPhotos"]
+		.sort((a, b) => b.volume - a.volume)
+		.sort((a, b) => b.issue - a.issue);
+	const articlesWithoutPhotos: ReceivedArticle[] = grouping["withoutPhotos"];
+	const num_articles_each_side = 5;
+
+	return (
+		<div id={styles.mixed_article_view}>
+			<section id={styles.left}>
+				{articlesWithoutPhotos
+					.slice(0)
+					.slice(0, num_articles_each_side)
+					.map((article, index) => (
+						<CenterArticle key={index} article={article} />
+					))}
+			</section>
+			<section id={styles.center}>
+				<div className={styles.top}>
+					{articlesWithPhotos
+						.slice(0)
+						.slice(0, 1)
+						.map((article_iterator, index) => (
+							<CenterArticle
+								key={index}
+								article={article_iterator}
+								display_image
+							/>
+						))}
+				</div>
+				<div className={styles.bottom}>
+					{articlesWithPhotos
+						.slice(0)
+						.slice(1, 3)
+						.map((article_iterator, index) => (
+							<CenterArticle
+								key={index}
+								article={article_iterator}
+								display_image
+							/>
+						))}
+				</div>
+			</section>
+			<section id={styles.right}>
+				{articlesWithoutPhotos
+					.slice(0)
+					.slice(num_articles_each_side, num_articles_each_side * 2)
+					.map((article, index) => (
+						<CenterArticle key={index} article={article} />
+					))}
+			</section>
+		</div>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,14 @@
 import Head from "next/head";
-import Link from "next/link";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { get_articles } from "../db";
-import {
-	ReceivedArticle,
-	DepartmentsArray,
-	DepartmentsArrayDisplay,
-} from "../ts_types/db_types";
-import Separator from "../components/Separator";
-import dateFromID from "../utils/dateFromID";
-import generate_contributors_jsx from "../components/GenerateContributorsJSX";
+import { ReceivedArticle } from "../ts_types/db_types";
+
+import MixedArticleDisplay from "../components/MixedArticleDisplay";
 interface Props {
 	articles: [ReceivedArticle];
 }
 
 const Home = (props: Props) => {
-	// console.log("Props: ", props);
-	const displayArticles: any[] = []; // Any type because this element will change often
-	const articles = props.articles;
-
-	// Find hero article
-	let heroArticleIndex = articles.length - 1; // Default to the last article
-	for (let i = articles.length - 1; i >= 0; i--) {
-		const article = articles[i];
-		if (article.cover_image_contributor) {
-			heroArticleIndex = i;
-			break;
-		}
-	}
-	let heroArticle = articles[heroArticleIndex];
-
-	articles
-		.slice(0) // .slice(0) first so that we make a copy of the articles array to mutate later with the second .splice()
-		.slice(0, heroArticleIndex)
-		.forEach((article_iterator, index) => {
-			displayArticles.push(
-				<div id={styles.article} key={String(article_iterator._id)}>
-					<Link passHref href={"/article/" + article_iterator.slug}>
-						<h2 id={styles.title} className="discrete-link">
-							{article_iterator.title}
-						</h2>
-					</Link>
-					<div
-						className={styles.authors}
-						style={{ fontFamily: "var(--secondary-font)" }}
-					>
-						{generate_contributors_jsx(
-							article_iterator.contributors
-						)}
-					</div>
-					<p id={styles.articleInfoDate}>
-						{dateFromID(article_iterator._id)}
-					</p>
-					<Link passHref href={"/article/" + article_iterator.slug}>
-						<p id={styles.summary} className="discrete-link">
-							{article_iterator.summary}
-						</p>
-					</Link>
-					<Link
-						href={`/department/${
-							DepartmentsArray[article_iterator.section_id]
-						}`}
-						passHref
-					>
-						<p
-							id={styles.articleInfo}
-							className="discrete-link"
-							style={{ fontFamily: "var(--secondary-font)" }}
-						>
-							{
-								DepartmentsArrayDisplay[
-									article_iterator.section_id
-								]
-							}
-						</p>
-					</Link>
-				</div>
-			);
-		});
-
 	return (
 		<div>
 			<Head>
@@ -88,87 +17,7 @@ const Home = (props: Props) => {
 			</Head>
 
 			<main id={styles.main}>
-				<div id={styles.landingScreen}>
-					<div id={styles.heroStory}>
-						<div id={styles.heroImageContainer}>
-							<Link
-								passHref
-								href={"/article/" + heroArticle.slug}
-							>
-								<Image
-									alt="big image"
-									id={styles.heroImage}
-									src={heroArticle.cover_image}
-									sizes="800px"
-									quality={90}
-									fill
-								/>
-							</Link>
-							<div id={styles.departmentBar}>
-								<Link
-									href={`/department/${
-										DepartmentsArray[heroArticle.section_id]
-									}`}
-									passHref
-								>
-									{
-										DepartmentsArrayDisplay[
-											heroArticle.section_id
-										]
-									}
-								</Link>
-							</div>
-						</div>
-
-						<Link passHref href={"/article/" + heroArticle.slug}>
-							<h1 className="discrete-link">
-								{heroArticle.title}
-							</h1>
-						</Link>
-
-						<div
-							className={styles.authors}
-							style={{
-								justifyContent: "center",
-							}}
-						>
-							{generate_contributors_jsx(
-								heroArticle.contributors
-							)}
-						</div>
-						<Link passHref href={"/article/" + heroArticle.slug}>
-							<p className="discrete-link" id={styles.summary}>
-								{heroArticle.summary}
-							</p>
-						</Link>
-						<Link passHref href={"/article/" + heroArticle.slug}>
-							<span>
-								<Image
-									id={styles.down_arrow}
-									alt="down arrow"
-									src="/images/down-arrow.svg"
-									width={24}
-									height={24}
-								/>
-							</span>
-						</Link>
-					</div>
-
-					<div id={styles.latestArticles}>
-						<h1 id={styles.heading}>Latest</h1>
-						<Separator />
-						<div>
-							{displayArticles
-								.slice(
-									displayArticles.length - 5,
-									displayArticles.length - 1
-								)
-								.reverse()}
-						</div>
-					</div>
-				</div>
-				<div id={styles.articles}></div>
-				<div id={styles.articles}></div>
+				<MixedArticleDisplay articles={props.articles} />
 			</main>
 		</div>
 	);
@@ -177,7 +26,7 @@ const Home = (props: Props) => {
 export default Home;
 
 export async function getServerSideProps() {
-	let articles = await get_articles();
+	let articles = await get_articles(20);
 	return {
 		props: { articles: JSON.parse(JSON.stringify(articles)) },
 	};
