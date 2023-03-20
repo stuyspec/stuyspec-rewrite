7078255910ff630f70d11f068516ce6f85004db6
leomet07 <leomet07@gmail.com>
2023-03-20 14:32:05 -0400

    Use new crimson education ad

--- advertisements.ts
+++ advertisements.ts
@@ -9,10 +9,10 @@ const advertisements = [
 		image_src: "/images/ads/dont_sweat_the_essay.png",
 		url: "https://www.dontsweattheessay.com/?utm_source=Stuyvesant&utm_medium=SpectatorWeb&utm_campaign=Spring2022",
 	},
-	// {
-	// 	name: "Crimson Education",
-	// 	image_src: "/images/ads/crimson_education.png",
-	// 	url: "https://www.crimsoneducation.org/us/",
-	// },
+	{
+		name: "Crimson Education",
+		image_src: "/images/ads/crimson_education.png",
+		url: "https://www.crimsoneducation.org/us/campaign/april-13-us-10-things-top-colleges-look-for-with-martin-walsh-registration/",
+	},
 ];
 export default advertisements;
