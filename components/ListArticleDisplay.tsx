ee7cd9473b90233cef4cac6a2b1a69a51d340e1d
leomet07 <leomet07@gmail.com>
2023-06-19 21:42:24 -0400

    Display department on ListArticleView and center left-side information

--- components/ListArticleDisplay.tsx
+++ components/ListArticleDisplay.tsx
@@ -1,23 +1,36 @@
 /* eslint-disable @next/next/no-img-element */
-import { ReceivedArticle, ReceivedStaff } from "../ts_types/db_types";
+import {
+	ReceivedArticle,
+	DepartmentsArray,
+	DepartmentsArrayDisplay,
+} from "../ts_types/db_types";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/ListArticleDisplay.module.css";
-import groupByImageExists from "../utils/groupArticles";
 import generate_contributors_jsx from "./GenerateContributorsJSX";
 
 export default function ListArticleDisplay(props: {
 	articles: ReceivedArticle[];
 }) {
-	const grouping = groupByImageExists(props.articles);
-	const articlesWithPhotos: ReceivedArticle[] = grouping["withPhotos"];
-	const articlesWithoutPhotos: ReceivedArticle[] = grouping["withoutPhotos"];
 	return (
 		<section id={styles.list_view}>
 			{props.articles.map((article) => (
 				<div className={styles.item} key={article._id as any}>
 					<div className={styles.inner_item}>
 						<div className={styles.item_left}>
+							<p className={styles.department + " discrete-link"}>
+								<Link
+									href={`/department/${
+										DepartmentsArray[article.section_id]
+									}`}
+								>
+									{
+										DepartmentsArrayDisplay[
+											article.section_id
+										]
+									}
+								</Link>
+							</p>
 							<h2>
 								<Link
 									href={"/article/" + article.slug}
