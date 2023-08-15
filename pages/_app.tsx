cb60f579f8d5876b682e1b455672b10c98440598
leomet07 <leomet07@gmail.com>
2023-08-15 15:20:14 -0400

    Add the necessary link tags for other favicon formats

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,6 +18,33 @@ function MyApp(props: AppProps) {
 		<div>
 			<Head>
 				<link rel="icon" href="/favicon.ico" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png?v=1.0"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png?v=1.0"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png?v=1.0"
+				/>
+				<link rel="manifest" href="/site.webmanifest?v=1.0" />
+				<link
+					rel="mask-icon"
+					href="/safari-pinned-tab.svg?v=1.0"
+					color="#5bbad5"
+				/>
+				<link rel="shortcut icon" href="/favicon.ico?v=1.0" />
+				<meta name="msapplication-TileColor" content="#da532c" />
+				<meta name="theme-color" content="#ffffff" />
+
 				<title>{title}</title>
 
 				{/* Meta tags must have keys so that NextJS can override/remove duplicate meta tags */}
