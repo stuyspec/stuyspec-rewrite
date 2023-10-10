cb77dfe85d37b299633c21c44e168a4a4bf56467
Amaryllis <asun50@stuy.edu>
2023-10-09 21:09:29 -0400

    Added borders to Recruitments page

--- db_conn.ts
+++ db_conn.ts
@@ -1,6 +1,6 @@
 // CREDIT: https://github.com/vercel/next.js/blob/canary/examples/with-mongodb/lib/mongodb.ts
 
-import { MongoClient } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 if (!process.env.MONGODB_URI) {
 	throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
@@ -10,7 +10,13 @@ declare global {
 	var _mongoClientPromise: Promise<MongoClient>;
 }
 const uri = process.env.MONGODB_URI;
-const options = {};
+const options =  {
+	// serverApi: {
+	// 	version: ServerApiVersion.v1,
+	// 	strict: true,
+	// 	deprecationErrors: true,
+	// }
+};
 
 let client;
 let clientPromise: Promise<MongoClient>;
