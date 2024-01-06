e94839bca00ce39f6a068c86ed41933d33c38821
Kyle Lee <klee50@stuy.edu>
2024-01-05 22:06:36 -0500

    Added rate limiter files middleware.ts, 429.ts, rate-limit.ts

---
+++ pages/api/429.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type ResponseData = {
+  message: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  return res.status(429).json({ message: 'Way too many requests' });
+}
---
+++ pages/api/rate-limit.ts
@@ -0,0 +1,65 @@
+import rateLimit from 'express-rate-limit'
+import slowDown from 'express-slow-down'
+const applyMiddleware = (middleware:any) => (request:any, response:any) => 
+  new Promise((resolve, reject) => {
+    middleware(request, response, (result:any) =>
+      result instanceof Error ? reject(result) : resolve(result)
+    )
+  })
+
+const getIP = (request:any) =>
+  request.ip ||
+  request.headers['x-forwarded-for'] ||
+  request.headers['x-real-ip'] ||
+  request.connection.remoteAddress ||
+  request.clientIp
+
+export const getRateLimitMiddlewares = ({
+  limit = 11,
+  windowMs = 60 * 1000,
+  delayAfter = Math.round(11 / 2),
+  delayMs = (hits:any) => hits * 1000,
+  //delayMs = 500,
+  maxDelayMs = 4000,
+  handler = function (request:any, response:any) {
+    const dummyData = { limit: 5, used: 5, remaining: -1, reset: '' }
+    return response.status(429).json(dummyData);
+  }
+} = {}) => [
+  slowDown({ keyGenerator: getIP, windowMs, delayAfter, delayMs }),
+  rateLimit({ keyGenerator: getIP, windowMs, max: limit, handler: handler }),
+]
+
+async function applyRateLimit( request:any , response:any) {
+  await Promise.all(
+    middlewares
+      .map(applyMiddleware)
+      .map(middleware => middleware(request, response))
+  )
+}
+
+//export default applyRateLimit
+
+const middlewares = getRateLimitMiddlewares({ limit: 11 }).map(applyMiddleware)
+
+export default async function handler(req:any, res:any) {
+
+  console.log(`rate-limit(IP: ${req.getIP}) called`)
+  try {
+    await Promise.all(
+      middlewares.map(middleware => middleware(req, res))
+    )
+  } catch {
+    console.log('rate-limit() catch called')
+    const dummyData = { limit: 5, used: 5, remaining: -1, reset: '' }
+    return res.status(429).json(dummyData);
+  }
+  const rateLimitValues = {
+    limit: req['rateLimit']['limit'],
+    used: req['rateLimit']['used'],
+    remaining: req['rateLimit']['remaining'],
+    reset: req['rateLimit']['resetTime']
+  }
+  //console.log('parsed ratelimit:', rateLimitValues)
+  return res.status(200).json(rateLimitValues);
+}
