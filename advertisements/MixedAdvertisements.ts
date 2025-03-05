3bf364678da1bc302f62b1b4f8092923d91e3fda
Max <146007357+Maxi-boop@users.noreply.github.com>
2025-03-04 21:19:38 -0500

    Update MixedAdvertisements.ts
    Cleared Ads

--- advertisements/MixedAdvertisements.ts
+++ advertisements/MixedAdvertisements.ts
@@ -3,25 +3,6 @@ const MixedAdvertisements: {
 	image_src: string;
 	url: string;
 }[] = [
-		{
-			name: "Achievable Act",
-			image_src: "/images/ads/achievable_act.png",
-			url: "https://app.achievable.me/study/act/purchase#p=Stuyvesant/",
-		},
-		{
-			name: "Achievable Act",
-			image_src: "/images/ads/achievable_act.png",
-			url: "https://app.achievable.me/study/act/purchase#p=Stuyvesant/",
-		},
-		{
-			name: "Ivy Prep",
-			image_src: "/images/ads/ivy_prep.png",
-			url: "http://ivyprepschool.com/",
-		},
-		{
-			name: "Ivy Prep",
-			image_src: "/images/ads/ivy_prep.png",
-			url: "http://ivyprepschool.com/",
-		},
+	
 	]; // duplicate ads cuz just 2 causes repeats on home page
 export default MixedAdvertisements;
