1581a4d661b5a0999ab3857b28d8242ee9b3a8cc
Max <146007357+Maxi-boop@users.noreply.github.com>
2025-03-28 22:41:51 -0400

    Fixed Ads

--- advertisements/BannerAdvertisements.ts
+++ advertisements/BannerAdvertisements.ts
@@ -3,6 +3,10 @@ const BannerAdvertisements: {
 	image_src: string;
 	url: string;
 }[] = [
-
+	{
+		name: "Kingfisher Prep",
+		image_src: "/images/ads/KingfisherPrep.png",
+		url: "https://www.kingfisherprep.com/post/five-tips-for-ivy-league-application-essays",
+	}
 	];
 export default BannerAdvertisements;
--- advertisements/MixedAdvertisements.ts
+++ advertisements/MixedAdvertisements.ts
@@ -3,5 +3,10 @@ const MixedAdvertisements: {
   image_src: string;
   url: string;
 }[] = [
+  {
+    name: "Lucent Education",
+    image_src: "/images/ads/LucentEd.png",
+    url: "https://www.lucenteducation.com/",
+  }
 ]; // duplicate ads cuz just 2 causes repeats on home page
 export default MixedAdvertisements;
