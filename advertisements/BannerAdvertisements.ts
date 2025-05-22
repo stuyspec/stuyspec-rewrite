640c2366e8163d58af2dc87258e1f8dc9d7235f0
Max <146007357+Maxi-boop@users.noreply.github.com>
2025-05-22 16:16:27 -0400

    A

--- advertisements/BannerAdvertisements.ts
+++ advertisements/BannerAdvertisements.ts
@@ -7,6 +7,11 @@ const BannerAdvertisements: {
 		name: "Kingfisher Prep",
 		image_src: "/images/ads/KingfisherPrep.png",
 		url: "https://www.kingfisherprep.com/post/five-tips-for-ivy-league-application-essays",
+	},
+	{
+		name: "Cooper Test Prep",
+		image_src: "/images/ads/CooperTestPrep.png",
+		url: "https://www.cooperprep.com/sat-classes?gad_source=1&gad_campaignid=21199327581",
 	}
 	];
 export default BannerAdvertisements;
