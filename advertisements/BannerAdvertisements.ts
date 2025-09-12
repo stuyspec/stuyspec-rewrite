2906a5739a2f4c4a453425e35ca25dc3aaa74bee
Max <146007357+Maxi-boop@users.noreply.github.com>
2025-09-11 20:42:57 -0400

    Added advertisements

--- advertisements/BannerAdvertisements.ts
+++ advertisements/BannerAdvertisements.ts
@@ -12,6 +12,11 @@ const BannerAdvertisements: {
 		name: "Cooper Test Prep",
 		image_src: "/images/ads/CooperTestPrep.png",
 		url: "https://www.cooperprep.com/sat-classes?gad_source=1&gad_campaignid=21199327581",
+	},
+	{
+		name: "Prepatory Test Prep",
+		image_src: "/images/ads/prepory banner.png",
+		url: "https://prepory.com/college-admissions-counselors/?kw=college%20counselor&cpn=21492505911&custom_campaign_name=21492505911&utm_term=college%20counselor&utm_campaign=luna_gsn_nonbrand&utm_source=adwords&utm_medium=ppc&hsa_acc=7922024019&hsa_cam=21492505911&hsa_grp=165132571117&hsa_ad=737024529518&hsa_src=g&hsa_tgt=kwd-353934885274&hsa_kw=college%20counselor&hsa_mt=b&hsa_net=adwords&hsa_ver=3&gad_source=1&gad_campaignid=21492505911",
 	}
 	];
 export default BannerAdvertisements;
--- advertisements/MixedAdvertisements.ts
+++ advertisements/MixedAdvertisements.ts
@@ -7,6 +7,14 @@ const MixedAdvertisements: {
     name: "Lucent Education",
     image_src: "/images/ads/LucentEd.png",
     url: "https://www.lucenteducation.com/",
+  }, {
+    name: "Prepory Elite Prep",
+    image_src: "/images/ads/prepory box.png",
+    url: "https://prepory.com/college-admissions-counselors/?kw=college%20counselor&cpn=21492505911&custom_campaign_name=21492505911&utm_term=college%20counselor&utm_campaign=luna_gsn_nonbrand&utm_source=adwords&utm_medium=ppc&hsa_acc=7922024019&hsa_cam=21492505911&hsa_grp=165132571117&hsa_ad=737024529518&hsa_src=g&hsa_tgt=kwd-353934885274&hsa_kw=college%20counselor&hsa_mt=b&hsa_net=adwords&hsa_ver=3&gad_source=1&gad_campaignid=21492505911",
+  }, {
+    name: "Kingfisher Prep",
+    image_src: "/images/ads/KFP Box.jpg",
+    url: "https://www.kingfisherprep.com/post/five-tips-for-ivy-league-application-essays" 
   }
 ]; // duplicate ads cuz just 2 causes repeats on home page
 export default MixedAdvertisements;
