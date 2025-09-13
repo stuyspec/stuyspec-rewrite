f32d0715619843d47b06c941376de956245c5ca8
Max <146007357+Maxi-boop@users.noreply.github.com>
2025-09-12 20:11:11 -0400

    Fixed image urls

--- advertisements/MixedAdvertisements.ts
+++ advertisements/MixedAdvertisements.ts
@@ -9,11 +9,11 @@ const MixedAdvertisements: {
     url: "https://www.lucenteducation.com/",
   }, {
     name: "Prepory Elite Prep",
-    image_src: "/images/ads/prepory box.png",
+    image_src: "/images/ads/preporybox.png",
     url: "https://prepory.com/college-admissions-counselors/?kw=college%20counselor&cpn=21492505911&custom_campaign_name=21492505911&utm_term=college%20counselor&utm_campaign=luna_gsn_nonbrand&utm_source=adwords&utm_medium=ppc&hsa_acc=7922024019&hsa_cam=21492505911&hsa_grp=165132571117&hsa_ad=737024529518&hsa_src=g&hsa_tgt=kwd-353934885274&hsa_kw=college%20counselor&hsa_mt=b&hsa_net=adwords&hsa_ver=3&gad_source=1&gad_campaignid=21492505911",
   }, {
     name: "Kingfisher Prep",
-    image_src: "/images/ads/KFP Box.jpg",
+    image_src: "/images/ads/KFPBox.jpg",
     url: "https://www.kingfisherprep.com/post/five-tips-for-ivy-league-application-essays" 
   }
 ]; // duplicate ads cuz just 2 causes repeats on home page
