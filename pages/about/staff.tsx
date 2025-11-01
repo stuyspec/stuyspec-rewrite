def9443c23f3e24aa86061fb0a7d18fa2f2c9712
Max <146007357+Maxi-boop@users.noreply.github.com>
2025-11-01 13:45:25 -0400

    Added features -Cleaned up Staff -Made card.module.css for the cards on the staff page -Made new global variables

--- pages/about/staff.tsx
+++ pages/about/staff.tsx
@@ -1,128 +1,373 @@
 import Head from "next/head";
 import styles from "../../styles/Staff.module.css";
+import cardStyles from "../../styles/Card.module.css";
 import { generateMetaTags } from "../../utils/generateMetaTags";
+import Link from "next/link";
+import Image from "next/image";
+import { useMemo } from "react";
+import unknownPhoto from "../../public/images/SpectatorCollageUnknown.png";
+import { StaticImageData } from "next/image";
 
+interface CardProps {
+  name: string;
+  department:
+    | "opinions"
+    | "arts and entertainment"
+    | "news"
+    | "features"
+    | "web"
+    | "business"
+    | "copy"
+    | "layout"
+    | "sports"
+    | "humor"
+    | "science"
+    | "photos"
+    | "art"
+    | "faculty";
+  //Department that the user is apart of.
+  position: "EIT" | "E";
+  /* 
+	NOTES
+	MB: Managing Board (NOT INCLUDED)
+	EIT: Editors-in-training
+	E: Editor
+	*/
+  photo?: StaticImageData | string;
+  //This string will be the src of the image in the memory.
+  //If the photo is not chosen, then a gradient will be set in the CSS
+}
+function Card(cardProps: CardProps) {
+  const urlLink = "/staff/".concat(
+    cardProps.name.toLowerCase().replace("-", " ").split(" ").join("-")
+  );
+  console.log(urlLink);
+  let imageSrcComponent;
+  if (cardProps.photo &&  typeof cardProps.photo === 'string') {
+    imageSrcComponent = {
+      backgroundImage: `url(${cardProps.photo})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    };
+  } else{
+    imageSrcComponent = null
+  }
+
+  return (
+    <article className={cardStyles.cardArea}>
+      {
+        // If the photo exists from the imageSrcComponent, then there will be a abckground iamge
+        // If it does not exist then a gradient will be chosen depending on the department
+      }
+      <Link href={urlLink}>
+        <div className={cardStyles.imageArea}>
+          {
+            //Renders the image component
+          }
+          {imageSrcComponent !== null ? (
+            <div style={imageSrcComponent} id={cardStyles.imagePhoto}></div>
+          ) : cardProps.photo !== null ? (
+            // TODO: WORK ON THE GRADIENT COLOR FOR EACH DEPARTMENT
+            <div id={cardStyles.imageStatic}>
+              <div id={cardStyles.overlay}></div>
+              <Image src={unknownPhoto} alt="unknown" id={cardStyles.image}/>
+            </div>
+          ) : (
+            <div id={cardStyles.imageGradient}></div>
+          )}{" "}
+        </div>
+      </Link>
+      <div className={cardStyles.textArea}>
+        {
+          //Creates the name and the description for the card
+        }
+        <p id={cardStyles.textName}>{cardProps.name}</p>
+        <p id={cardStyles.textPosition}>
+          {
+            // PRINTS OUT THE POSITION OF THE MEMBER
+            // FIRST UPPERCASES THEIR DEPARTMENT AND THEN APPENDS THEIR POSITION
+            // web Department Editor -> Web Department Editor
+          }
+          {cardProps.department !== "faculty"
+            ? cardProps.department.charAt(0).toUpperCase() +
+              cardProps.department.substring(1).concat(`          Department
+          ${cardProps.position === "E" ? "Editor" : "Editor-in-Training"}`)
+            : "Faculty Advisor"}{" "}
+        </p>
+      </div>
+    </article>
+  );
+}
+function cardGen() {
+  //! IMPORTANT TODO
+  //TODO: MAKE SOMETHING THAT GRABS THIS INFORMATION FROM THE DATABASE RATHER THAN US HARD CODING IT
+  //TODO: WILL SAVE A LOT OF TIME.
+  const staff: CardProps[] = [
+    {
+      name: "Myles Vuong",
+      position: "E",
+      department: "news",
+    },
+    {
+      name: "Hifza Kaleem",
+      position: "E",
+      department: "features",
+    },
+    {
+      name: "Dinara Gargu",
+      position: "E",
+      department: "news",
+    },
+    {
+      name: "Brendan Tan",
+      position: "E",
+      department: "news",
+    },
+    {
+      name: "Lauren Yang",
+      position: "E",
+      department: "news",
+    },
+    {
+      name: "William Chen",
+      position: "EIT",
+      department: "features",
+    },
+    {
+      name: "Ada Gordon",
+      position: "E",
+      department: "features",
+    },
+    {
+      name: "Grace Jung",
+      position: "E",
+      department: "features",
+    },
+    {
+      name: "Leah Riegel",
+      position: "E",
+      department: "features",
+    },
+    {
+      name: "Noa Salas Adam",
+      position: "EIT",
+      department: "features",
+    },
+    {
+      name: "Dean Hevenstone",
+      position: "E",
+      department: "opinions",
+    },
+    {
+      name: "Joanne Hwang",
+      position: "E",
+      department: "opinions",
+    },
+    {
+      name: "Stella Krajka",
+      position: "E",
+      department: "opinions",
+    },
+    {
+      name: "Evelyn Lifton",
+      position: "EIT",
+      department: "opinions",
+    },
+    {
+      name: "Aarya Balakrishnan",
+      position: "E",
+      department: "science",
+    },
+    {
+      name: "Isabel Cho",
+      position: "EIT",
+      department: "science",
+    },
+    {
+      name: "Sonya Cisse",
+      position: "E",
+      department: "science",
+    },
+    {
+      name: "Narnia Poddar",
+      position: "EIT",
+      department: "science",
+    },
+    {
+      name: "Benson Chen",
+      position: "E",
+      department: "arts and entertainment",
+    },
+    {
+      name: "Galen Jack",
+      position: "E",
+      department: "arts and entertainment",
+    },
+    {
+      name: "Emile Lee-Suk",
+      position: "E",
+      department: "arts and entertainment",
+    },
+    {
+      name: "Somerset Seidenberg",
+      position: "E",
+      department: "arts and entertainment",
+    },
+    {
+      name: "Selina Lin",
+      position: "E",
+      department: "humor",
+    },
+    {
+      name: "Alexis Qian",
+      position: "E",
+      department: "humor",
+    },
+    {
+      name: "Nina Benson",
+      position: "EIT",
+      department: "sports",
+    },
+    {
+      name: "Elijah Choi",
+      position: "E",
+      department: "sports",
+    },
+    {
+      name: "Leonardo Guidi",
+      position: "E",
+      department: "sports",
+    },
+    {
+      name: "Saif Iftikhar",
+      position: "EIT",
+      department: "sports",
+    },
+    {
+      name: "Boone Ireland",
+      position: "E",
+      department: "sports",
+    },
+    {
+      name: "Eva Kastoun",
+      position: "E",
+      department: "photos",
+    },
+    {
+      name: "Emma Nakhle",
+      position: "E",
+      department: "photos",
+    },
+    {
+      name: "Karina Huang",
+      position: "E",
+      department: "art",
+    },
+    {
+      name: "Yuma Kono",
+      position: "EIT",
+      department: "art",
+    },
+    {
+      name: "Rhea Malhorta",
+      position: "E",
+      department: "art",
+    },
+    {
+      name: "Lixin Zhang",
+      position: "E",
+      department: "art",
+    },
+    {
+      name: "Anjali Bechu",
+      position: "E",
+      department: "layout",
+    },
+    {
+      name: "Elysia Chen",
+      position: "E",
+      department: "layout",
+    },
+    {
+      name: "Isabel Noa",
+      position: "E",
+      department: "layout",
+    },
+    {
+      name: "Karen Xu",
+      position: "E",
+      department: "layout",
+    },
+    {
+      name: "Naomi Hsieh",
+      position: "E",
+      department: "copy",
+    },
+    {
+      name: "Yuna Lee",
+      position: "E",
+      department: "copy",
+    },
+    {
+      name: "Emma Lin",
+      position: "E",
+      department: "copy",
+    },
+    {
+      name: "Fiona Cai",
+      position: "E",
+      department: "business",
+    },
+    {
+      name: "Anderson Oh",
+      position: "E",
+      department: "business",
+    },
+    {
+      name: "Everett Yu-Dawidowicz",
+      position: "E",
+      department: "business",
+    },
+    {
+      name: "Arnav Malhorta",
+      position: "EIT",
+      department: "web",
+    },
+    {
+      name: "Maximiliano Pettica",
+      position: "E",
+      department: "web",
+    },
+    {
+      name: "Owen Shi",
+      position: "E",
+      department: "web",
+    },
+  ];
+  return staff.map((e, _) => {
+    return (
+      <Card name={e.name} department={e.department} position={e.position} photo={unknownPhoto}/>
+    );
+  });
+}
 function StaffPage() {
-	const page_title = "Staff - The Stuyvesant Spectator";
-	const meta_url = `https://stuyspec.com/about/staff`;
-	const meta_description = `The members of The Stuyvesant Spectator's 2024-2025 Editorial Board.`;
+  const page_title = "Staff - The Stuyvesant Spectator";
+  const meta_url = `https://stuyspec.com/about/staff`;
+  const meta_description = `The members of The Stuyvesant Spectator's 2024-2025 Editorial Board.`;
 
-	return (
-		<>
-			<Head>
-				{generateMetaTags(page_title, meta_description, meta_url)}
-			</Head>
-			<main id={styles.main}>
-				<h1>Members of the 2023-2024 Spectator Editorial Board</h1>
-				<section id={styles.departments}>
-					<p className={styles.special_def}>* Managing Board</p>
-					<p className={styles.special_def}>** Editors-in-Training</p>
+  const cards = cardGen()
 
-					<div>
-						<h3 className={styles.EIC}>Editors In Chief</h3>
-						<p>Suyeon Ryu*</p>
-						<p>Khush Wadhwa*</p>
-					</div>
-					<div>
-						<h3 className={styles.news}>News Editors</h3>
-						<p>Talia Arcasoy*</p>
-						<p>Seth Fenton*</p>
-						<p>Zoey Marcus</p>
-						<p>Brendan Tan</p>
-						<p>Lauren Yang</p>
-					</div>
-					<div>
-						<h3 className={styles.features}>Features Editors</h3>
-						<p>Abigail Jin</p>
-						<p>Hifza Kaleem</p>
-						<p>Cathleen Xi</p>
-					</div>
-					<div>
-						<h3 className={styles.ops}>Opinions Editors</h3>
-						<p>Joanne Hwang</p>
-						<p>Helen Mancini*</p>
-						<p>Amaryllis Sun</p>
-						<p>Myles Vuong</p>
-					</div>
-					<div>
-						<h3 className={styles.science}>Science Editors</h3>
-						<p>Aarya Balakrishnan</p>
-						<p>Sonya Cisse</p>
-						<p>Ryan Lin</p>
-						<p>Michelle Ng</p>
-					</div>
-					<div>
-						<h3 className={styles.ae}>Arts & Entertainment Editors</h3>
-						<p>Benson Chen</p>
-						<p>Zoë Feigelson</p>
-						<p>Madeline Hutchinson</p>
-						<p>Emile Lee-Suk</p>
-						<p>Santino Suarez*</p>
-					</div>
-					<div>
-						<h3 className={styles.humor}>Humor Editors</h3>
-						<p>Michelle “Mike” Huang</p>
-						<p>Munem Tajwar</p>
-					</div>
-					<div>
-						<h3 className={styles.sports}>Sports Editors</h3>
-						<p>Elijah Choi</p>
-						<p>Leonardo Guidi</p>
-						<p>Kaileen So</p>
-						<p>Evan Wong</p>
-						<p>Duncan Park</p>
-					</div>
-					<div>
-						<h3 className={styles.photos}>Photography Editors</h3>
-						<p>Geoffrey Huang</p>
-						<p>Ibtida Khurshed</p>
-						<p>Honora Muratori</p>
-
-					</div>
-					<div>
-						<h3 className={styles.art}>Art Directors</h3>
-						<p>Jaden Bae</p>
-						<p>Stacey Chen</p>
-						<p>Rhea Malhotra</p>
-						<p>Chuer Zhong</p>
-					</div>
-					<div>
-						<h3 className={styles.layout}>Layout Editors</h3>
-						<p>Anjali Bechu**</p>
-						<p>Elaine Liu</p>
-						<p>Isabel Noh**</p>
-						<p>Andre Wang</p>
-						<p>Karen Xu</p>
-						<p>Jasper Yu-Dawidowicz</p>
-					</div>
-					<div>
-						<h3 className={styles.copy}>Copy Editors</h3>
-						<p>Yuna Lee</p>
-						<p>Ryan Park</p>
-						<p>Niamh Werner</p>
-					</div>
-					<div>
-						<h3 className={styles.biz}>Business Managers</h3>
-						<p>Fiona Cai</p>
-						<p>Christopher Louie</p>
-						<p>Anderson Oh</p>
-						<p>Everett Yu-Dawidowicz**</p>
-					</div>
-					<div>
-						<h3 className={styles.web}>Web Editors</h3>
-						<p>Adam Choi</p>
-						<p>Angela Mashuryan</p>
-						<p>Lenny Metlitsky*</p>
-						<p>Ankita Saha</p>
-					</div>
-					<div>
-						<h3 className={styles.faculty}>Faculty Advisor</h3>
-						<p>Kerry Garfinkel</p>
-					</div>
-				</section>
-			</main>
-		</>
-	);
-};
+  return (
+    <>
+      <Head>{generateMetaTags(page_title, meta_description, meta_url)}</Head>
+      <main id={styles.main}>
+        <div id={styles.headingText}>
+          <h1>Members of the Spectator Editorial Board</h1>
+        </div>
+        <section id={styles.memberGrid}>{cards}</section>
+      </main>
+    </>
+  );
+}
 
 export default StaffPage;
