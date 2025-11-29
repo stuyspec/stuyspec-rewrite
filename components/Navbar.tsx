2d4335609278a0856bc04dea50e2dc5665d05744
Max <146007357+Maxi-boop@users.noreply.github.com>
2025-11-28 19:38:26 -0500

    FIXED -- SIDEBAR BEING ABLE TO SCROLL BACKGROUND WHEN SIDE BAR IS ACTIV
    CURRENTLY TESTING:
    - MOBILE INTERFACE FOR IOS DEVICES AND SHRINKING BAR UNDERNEATH`

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -304,19 +304,21 @@ const Navbar = () => {
               </div>
             </div>
 
-            <dialog className={styles.navBarSideBar} open={viewSubSection}>
-              <form method="dialog">
-                <button autoFocus onClick={toggleMenu} id={styles.closeButton}>
-                  <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
-                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
-                  </svg>
-                </button>
-              </form>
-              <Sidebar
-                showSidebar={viewSubSection}
-                setShowSidebar={setViewSubSection}
-              />
-            </dialog>
+            <div className={styles.dialogContainer}>
+              <dialog className={styles.navBarSideBar} open={viewSubSection}>
+                <form method="dialog">
+                  <button autoFocus onClick={toggleMenu} id={styles.closeButton}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+                      <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                    </svg>
+                  </button>
+                </form>
+                <Sidebar
+                  showSidebar={viewSubSection}
+                  setShowSidebar={setViewSubSection}
+                />
+              </dialog>
+            </div>
           </nav>
 
           <div
