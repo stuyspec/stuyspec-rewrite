53e5d78cb7723e8c71f5001c98787100a6210591
Max <146007357+Maxi-boop@users.noreply.github.com>
2026-04-02 11:11:24 -0400

    Added changes to advertisements

--- advertisements/MixedAdvertisements.ts
+++ advertisements/MixedAdvertisements.ts
@@ -9,24 +9,24 @@ const MixedAdvertisements: {
     url: "https://www.zuckersbagels.com/",
   },
   {
+    name: "King Fisher Prepatory School",
+    image_src: "/images/ads/KingFisherBox.jpg",
+    url: "https://www.kingfisherprep.com/sat-act-college-application-coaching-for-stuyvesant-students",
+  },
+  {
     name: "Revolution Driving",
     image_src: "/images/ads/revolutiondriving.png",
     url: "https://revolutiondrivingschools.com/",
   },
-    {
+  {
     name: "Five Iron Golf",
     image_src: "/images/ads/FiveIronGolfBox.png",
     url: "https://fiveirongolf.com/book-now",
   },
   {
-    name: "Write The World",
-    image_src: "/images/ads/write-the-world.png",
-    url: "https://workshops.writetheworld.org/college-essay/?utm_campaign=23841340-Stuyvesant%20Spectator&utm_source=ads",
-  },
-  {
     name: "Prepory Elite Prep",
     image_src: "/images/ads/preporybox.png",
     url: "https://prepory.com/college-admissions-counselors/?kw=college%20counselor&cpn=21492505911&custom_campaign_name=21492505911&utm_term=college%20counselor&utm_campaign=luna_gsn_nonbrand&utm_source=adwords&utm_medium=ppc&hsa_acc=7922024019&hsa_cam=21492505911&hsa_grp=165132571117&hsa_ad=737024529518&hsa_src=g&hsa_tgt=kwd-353934885274&hsa_kw=college%20counselor&hsa_mt=b&hsa_net=adwords&hsa_ver=3&gad_source=1&gad_campaignid=21492505911",
-  }
+  },
 ]; // duplicate ads cuz just 2 causes repeats on home page
 export default MixedAdvertisements;
