443712ea99cfc0e4df09c94b6d96294078a9454f
Native315 <45272908+native315@users.noreply.github.com>
2026-09-15 17:20:22 -0400

    remove zucker's, revolution driving school, and prepory ads

--- advertisements/MixedAdvertisements.ts
+++ advertisements/MixedAdvertisements.ts
@@ -4,29 +4,14 @@ const MixedAdvertisements: {
   url: string;
 }[] = [
   {
-    name: "Zuckers",
-    image_src: "/images/ads/ZuckersBox.png",
-    url: "https://www.zuckersbagels.com/",
-  },
-  {
     name: "King Fisher Prepatory School",
     image_src: "/images/ads/KingFisherBox.jpg",
     url: "https://www.kingfisherprep.com/sat-act-college-application-coaching-for-stuyvesant-students",
   },
   {
-    name: "Revolution Driving",
-    image_src: "/images/ads/revolutiondriving.png",
-    url: "https://revolutiondrivingschools.com/",
-  },
-  {
     name: "Five Iron Golf",
     image_src: "/images/ads/FiveIronGolfBox.png",
     url: "https://fiveirongolf.com/book-now",
-  },
-  {
-    name: "Prepory Elite Prep",
-    image_src: "/images/ads/preporybox.png",
-    url: "https://prepory.com/college-admissions-counselors/?kw=college%20counselor&cpn=21492505911&custom_campaign_name=21492505911&utm_term=college%20counselor&utm_campaign=luna_gsn_nonbrand&utm_source=adwords&utm_medium=ppc&hsa_acc=7922024019&hsa_cam=21492505911&hsa_grp=165132571117&hsa_ad=737024529518&hsa_src=g&hsa_tgt=kwd-353934885274&hsa_kw=college%20counselor&hsa_mt=b&hsa_net=adwords&hsa_ver=3&gad_source=1&gad_campaignid=21492505911",
   },
 ]; // duplicate ads cuz just 2 causes repeats on home page
 export default MixedAdvertisements;
