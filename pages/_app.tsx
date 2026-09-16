2d29092b8f11ffb905bc768b085d09fda22622d8
Native315 <45272908+native315@users.noreply.github.com>
2026-09-15 21:49:57 -0400

    just give up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -85,14 +85,14 @@ function MyApp(props: AppProps) {
 			<Footer />
 			<Script
 				async
-				src="https://umami.stuyspec.com/script.js?id=webstats"
+				src="https://umami.stuyspec.com/script.js"
 				data-website-id="37284781-2b87-4063-8035-5a7f3e7ab6d3"
 			></Script>
-			<Script
+			{/*<Script
 				async
 				src="https://umami.stuyspec.com/script.js?id=bizstats"
 				data-website-id="6be609cd-6bd5-418f-8698-fa703f404c25"
-			></Script>
+			></Script>*/}
 		</ThemeProvider>
 	);
 }
