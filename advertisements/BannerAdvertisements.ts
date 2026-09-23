524484ed1bddae9f00c755f8ed37375c1ccca0e3
Hana Fathony <hana.ff.fathony@gmail.com>
2026-09-23 18:11:59 -0400

    removed three ads

--- advertisements/BannerAdvertisements.ts
+++ advertisements/BannerAdvertisements.ts
@@ -4,21 +4,6 @@ const BannerAdvertisements: {
   url: string;
 }[] = [
   {
-    name: "Cooper Test Prep",
-    image_src: "/images/ads/CooperTestPrep.png",
-    url: "https://www.cooperprep.com/sat-classes?gad_source=1&gad_campaignid=21199327581",
-  },
-  {
-    name: "Spark Admissions For Stuyvesant Families",
-    image_src: "/images/ads/sparkadmissions.jpg",
-    url: "https://www.sparkadmissions.com/stuyvesant/?utm_source=stuyvesant&utm_medium=banner&utm_campaign=stuyvesant_banner_ad",
-  },
-  {
-    name: "Ivy Wise Pro Bono Scholars Program",
-    image_src: "/images/ads/IvyWiseBanner.jpg",
-    url: "https://www.ivywise.com/about-ivywise/ivywise-gives-back/ivywise-scholars/?utm_source=stuyvesant&utm_medium=advertisement&utm_campaign=may+2026&utm_content=digital"
-  },
-  {
     name: "Prime Academics",
     image_src: "/images/ads/PrimeAcademics.png",
     url: "https://www.primeacademics.nyc/",
